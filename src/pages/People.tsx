a6e9c1798613da8a38e95d4676ae2f7e3c52928f
Nirmal-zigma <nirmal@zigmaglobal.in>
2026-04-20 13:50:19 +0530

    people at zigma hero image change

--- src/pages/People.tsx
+++ src/pages/People.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
 import Reveal from "@/components/animation/Reveal";
-import heroImage from "@/assets/people at zigma/Zigma beyond work/38.png";
+import heroImage from "@/assets/people at zigma/Zigma beyond work/39.jpeg";
 
 gsap.registerPlugin(ScrollTrigger);
 
